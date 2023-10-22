f5f2a9f547617bd1a261227a150828b72cf55324
Maya Ashok <mayaashok@wirelessprv-10-193-221-206.near.illinois.edu>
2023-10-22 14:53:58 -0500

    Commented stuff out

--- solar_axis_app/lib/theme.dart
+++ solar_axis_app/lib/theme.dart
@@ -15,9 +15,9 @@ class AppColors {
 
 ColorScheme colorScheme = const ColorScheme.dark(
   primary: AppColors.pink,
-  primaryVariant: AppColors.pink,
+  //primaryVariant: AppColors.pink,
   secondary: AppColors.orange,
-  secondaryVariant: AppColors.orange,
+  //secondaryVariant: AppColors.orange,
   surface: AppColors.tan,
   background: AppColors.tan,
 );
