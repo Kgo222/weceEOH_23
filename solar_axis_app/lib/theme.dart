2d8c9027ba0ff65b242e747669c7e98c4c6b8374
Kate <xinranh6@illinois.edu>
2023-11-05 15:18:44 -0600

    1105

--- solar_axis_app/lib/theme.dart
+++ solar_axis_app/lib/theme.dart
@@ -8,16 +8,16 @@ import 'package:flutter/material.dart';
 class AppColors {
   // Flutter colors are RGB color codes prefixed with an opacity value.
   // The FF represents full opacity
-  static const purple = Color(0xB4A7D6FF);
+  static const orange = Color(0xB4FF5321);
   static const tan = Color(0xFFEEDEC9);
   static const pink = Color(0xFFF7BDBE);
 }
 
 ColorScheme colorScheme = const ColorScheme.dark(
   primary: AppColors.pink,
-  primaryVariant: AppColors.pink,
-  secondary: AppColors.purple,
-  secondaryVariant: AppColors.purple,
+  //primaryVariant: AppColors.pink,
+  secondary: AppColors.orange,
+  //secondaryVariant: AppColors.orange,
   surface: AppColors.tan,
   background: AppColors.tan,
 );
