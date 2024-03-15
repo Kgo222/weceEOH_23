b39f339c290b4f85667cdc69f03e2e983ed2d10b
Kaitlin Gowens <gokaitgo100@gmail.com>
2024-03-15 17:19:01 -0500

    example of direction function idea

--- SolarFlowerCode_bluetoothAdded/directions.h
+++ SolarFlowerCode_bluetoothAdded/directions.h
@@ -1,19 +1,58 @@
 
 
 void changeDirection(String newDirection, String currDirection){
-  if(newDirection == "W"){
-    
+  if(newDirection.equals("W")){
+    if(currDirection.equals("NW") || currDirection.equals("N")){ //NW --> W or N --> W
+      
+      }
+    else{ //any other direction -> W
+      
+      }
     }
-  else if(newDirection == "NW"){
-    
+  else if(newDirection.equals("NW")){
+    if(currDirection.equals("W")){ // W --> NW
+      
+      }
+    else if(currDirection.equals("N")){ // N --> NW
+      
+      }
+    else{ //any other direction --> NW
+      }
     }
-  else if(newDirection == "N"){
-    
+  else if(newDirection.equals("N")){
+    if(currDirection.equals("NW") || currDirection.equals("W")){ // NW --> N or W --> N
+      
+      }
+    else{ //any other direction --> N
+      
+      }
     }
-  else if(newDirection == "W"){
-    
+  else if(newDirection.equals("W")){
+     if(currDirection.equals("SW")|| currDirection.equals("S")){ // SW --> W or S -->W
+      
+      }
+      else{ // any other direction --> W
+        
+        }
     }
-  else if(newDirection == "NW"){
-    
+  else if(newDirection.equals("SW")){
+    if(currDirection.equals("W")){ // W --> SW
+      
+      }
+    else if(currDirection.equals("S")){ // S --> SW
+      
+      }
+    else{ //any other direction --> SW
+      
+      }
     }
+  else if(newDirection.equals("S")){
+    if(currDirection.equals("SW") || currDirection.equals("W")){ // SW --> S or W --> S
+      
+      }
+     else{ //any other direction --> S
+      
+      }
+    }
+  else
   }
